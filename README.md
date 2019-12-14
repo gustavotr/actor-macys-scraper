4c5138d7aa87886e0dacd3f0fbc7c9a982be4143
Gustavo T. Rudiger <gustavotrudiger@gmail.com>
2019-12-14 11:45:05 -0300

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# Apify project template
+### Macys Scraper
 
-Before you start writing your first actor, please read [Apify Marketplace developers WIKI](https://gitlab.com/apify-public/wiki/wikis/home).
+Macys Scraper is an [Apify actor](https://apify.com/actors) for extracting data about actors from [Macys](https://macys.com/). It allows you to extract all products. It is build on top of [Apify SDK](https://sdk.apify.com/) and you can run it both on [Apify platform](https://my.apify.com) and locally.
+
+- [Input](#input)
+- [Output](#output)
+- [Compute units consumption](#compute-units-consumption)
+- [Extend output function](#extend-output-function)
+
+### Input
+
+| Field | Type | Description | Default value
+| ----- | ---- | ----------- | -------------|
+| startUrls | array | List of [Request](https://sdk.apify.com/docs/api/request#docsNav) objects that will be deeply crawled. The URL can be home page like `https://macys.com/` or category page `https://www.macys.com/shop/furniture/living-room-furniture?id=35319&cm_sp=c2_1111US_catsplash_furniture-_-row3-_-icon_living-room&edge=hybrid` or detail page `https://www.macys.com/shop/product/radley-fabric-sectional-sofa-collection-created-for-macys?ID=1101381&CategoryID=35319`. | `["https://macys.com/"]`|
+| maxItems | number | Maximum number of products that will be scraped | all found |
+| search | string | Keyword that will be used to search Macys`s products |  |
+| extendOutputFunction | string | Function that takes a Cheerio handle ($) as argument and returns data that will be merged with the result output. More information in [Extend output function](#extend-output-function) | |
+| proxyConfiguration | object | Proxy settings of the run. This actor works better with the Apify proxy group SHADER. If you have access to this Apify proxy group, leave the default settings. If not, you can use other Apify proxy groups or you can set `{ "useApifyProxy": false" }` to disable proxy usage | `{"useApifyProxy": true, "apifyProxyGroups": ["SHADER"] }`|
+
+### Output
+
+Output is stored in a dataset. Each item is an information about a product. Example:
+
+```json
+{}
+```
+
+### Compute units consumption
+Keep in mind that it is much more efficient to run one longer scrape (at least one minute) than more shorter ones because of the startup time.
+
+The average consumption is **0.2 Compute unit for 1000 actor pages** scraped
+
+### Extend output function
+
+You can use this function to update the result output of this actor. This function gets a Cheerio handle `$` as an argument so you can choose what data from the page you want to scrape. The output from this will function will get merged with the result output.
+
+The return value of this function has to be an object!
+
+You can return fields to achive 3 different things:
+- Add a new field - Return object with a field that is not in the result output
+- Change a field - Return an existing field with a new value
+- Remove a field - Return an existing field with a value `undefined`
+
+
+```js
+($) => {
+  return {
+        saleEnd: $('._3p7kp').text().trim(),
+        salePrice: 0,
+        url: undefined
+    }
+}
+```
+This example will add a new field `saleEnd`, change the `salePrice` field and remove `url` field
+```json
+{
+
+}
+```
--- src/main.js
+++ src/main.js
@@ -1,45 +1,70 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
-
-// Include Apify SDK. For more information, see https://sdk.apify.com/
 const Apify = require('apify');
+const cheerio = require('cheerio');
 
-const rp = require('request-promise');
+const { parseMainPage, parseCategory, getUrlType, parseProduct, EnumURLTypes } = require('./tools');
+
+
+const { log } = Apify.utils;
+log.setLevel(log.LEVELS.DEBUG);
 
 Apify.main(async () => {
-    // Get input of the actor (here only for demonstration purposes).
-    // If you'd like to have your input checked and have Apify display
-    // a user interface for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
     const input = await Apify.getInput();
-    console.log('Input:');
-    console.dir(input);
+    const { proxy, startUrls } = input;
 
-    if (!input || !input.sources) throw new Error('Input must be a JSON object with the "sources" field!');
+    const requestQueue = await Apify.openRequestQueue();
+    await Promise.all(startUrls.map((url) => {
+        const type = getUrlType(url);
+        requestQueue.addRequest({ url, userData: { type } });
+    }));
 
-    const requestList = await Apify.openRequestList('my-request-list', input.sources);
+    const crawler = new Apify.BasicCrawler({
+        requestQueue,
+        useSessionPool: true,
+        maxRequestsPerCrawl: 50,
 
-    // Create a basic crawler that will use request-promise to download
-    // web pages from a given list of URLs
-    const basicCrawler = new Apify.BasicCrawler({
-        requestList,
-        handleRequestFunction: async ({ request }) => {
-            await Apify.pushData({
-                request,
-                finishedAt: new Date(),
-                html: await rp(request.url),
-                '#debug': Apify.utils.createRequestDebugInfo(request),
-            });
+        handleRequestFunction: async ({ request, session }) => {
+            log.info(`Processing ${request.url}...`);
+
+            const requestOptions = {
+                url: request.url,
+                proxyUrl: Apify.getApifyProxyUrl({
+                    groups: proxy.apifyProxyGroups,
+                    session: session.id,
+                }),
+            };
+            const { body, headers } = await Apify.utils.requestAsBrowser(requestOptions);
+            const $ = cheerio.load(body);
+            const title = $('title').text();
+
+            if (title === 'Access Denied') {
+                session.markBad();
+                throw new Error('Access Denied');
+            }
+
+            session.markGood();
+
+            const { type } = request.userData;
+
+            if (type === EnumURLTypes.START_URL) {
+                log.debug('Start url...');
+                await parseMainPage({ requestQueue, $, body });
+            }
+
+            if (type === EnumURLTypes.CATEGORY) {
+                log.debug('Category url...');
+                await parseCategory({ requestQueue, $, body, userData: { ...request.userData } });
+            }
+
+            if (type === EnumURLTypes.PRODUCT) {
+                log.debug('Product url...');
+                await parseProduct({ requestQueue, $, body, userData: { ...request.userData, headers } });
+            }
         },
 
         handleFailedRequestFunction: async ({ request }) => {
-            await Apify.pushData({
-                '#isFailed': true,
-                '#debug': Apify.utils.createRequestDebugInfo(request),
-            });
+            console.log(`Request ${request.url} failed too many times`);
         },
     });
 
-    await basicCrawler.run();
+    await crawler.run();
 });
